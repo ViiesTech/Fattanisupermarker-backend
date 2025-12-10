1309675b96b63404d5701f49a879a669f5e608a1
ViiesTech <visstechapps@gmail.com>
2025-12-10 23:18:33 +0500

    add google login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -669,7 +669,7 @@ async function googleLogin(req, res) {
 
     const newUser = await UserModel.create({
       googleId, name, email, password: '', phone: '',
-      FCMToken: '', isGoogleUser: true
+      FCMToken: '', isGoogleUser: true  
     });
 
     const token = JWT.sign(
